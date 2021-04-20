c30cd38209725aec7ecda4f92337580fba37fadd
Thanathip Suwannakhot <78087668+danyouknowme@users.noreply.github.com>
2021-04-20 19:53:00 +0700

    Create FindingThePercentage.py

---
+++ Python/Easy/FindingThePercentage.py
@@ -0,0 +1,9 @@
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
+out = sum(student_marks[query_name]) / len(student_marks[query_name])
+print(f"{out:.2f}")
