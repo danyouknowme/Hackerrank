9fdb4862b4b43affa412bbbea1e2e6114c09de19
Thanathip Suwannakhot <78087668+danyouknowme@users.noreply.github.com>
2021-04-20 19:47:43 +0700

    Create IfElse.py

---
+++ Python/Easy/IfElse.py
@@ -0,0 +1,10 @@
+n = int(input().strip())
+if n % 2 != 0:
+    print("Weird")
+else:
+    if 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    elif n > 20:
+        print("Not Weird")
