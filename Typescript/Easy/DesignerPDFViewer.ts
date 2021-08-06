385e51c16828554d0b836a59a84a58e0f3915ec7
Thanathip Suwannakhot <78087668+danyouknowme@users.noreply.github.com>
2021-08-06 23:32:18 +0700

    Create DesignerPDFViewer.ts

---
+++ Typescript/Easy/DesignerPDFViewer.ts
@@ -0,0 +1,55 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'designerPdfViewer' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h
+ *  2. STRING word
+ */
+
+function designerPdfViewer(h: number[], word: string[]): number {
+    // Write your code here
+    return [...word]
+        .map(e => h[e.charCodeAt(0) - 97])
+        .reduce((a, b) => Math.max(a, b))
+        * word.length;
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const h: number[] = readLine().replace(/\s+$/g, '').split(' ').map(hTemp => parseInt(hTemp, 10));
+
+    const word: string[] = readLine().split("");
+
+    const result: number = designerPdfViewer(h, word);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
